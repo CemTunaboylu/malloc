30c4dadcb85e5b437ea27d2157f295d23efb9ece
Cem Tunaboylu <cemtunaboylu9@gmail.com>
2025-11-15 16:35:07 +0300

    append new probes to probe header

--- tests/probe.h
+++ tests/probe.h
@@ -8,3 +8,7 @@ block _mm_block_header(void);
 size_t _mm_bytes_obtained_from_os(void);
 size_t _mm_free_blocks(void);   // current free blocks
 size_t _mm_non_free_blocks(void);   // current non-free blocks
+size_t _mm_total_blocks(void);   // current free blocks
+void _mm_tear_down_allocator(void);
+
+void print_list_into_file(FILE* f);
