3d08489151d8fbf47a8738ffbf2c148e1909e7f3
Cem Tunaboylu <cemtunaboylu9@gmail.com>
2025-11-15 16:42:47 +0300

    change debug assertions to counters

--- src/mm_debug.h
+++ src/mm_debug.h
@@ -6,12 +6,12 @@
   #define MM_UNREACHABLE() assert(!"unreachable")
 
   int malloc_called;
-  #define MM_MALLOC_CALL() (malloc_called = 1)
+  #define MM_MALLOC_CALL() (malloc_called += 1)
   #define MM_RESET_MALLOC_CALL_MARKER() (malloc_called = 0)
   #define MM_ASSERT_MALLOC_CALLED(times) assert(malloc_called == times)
 
   int free_called;
-  #define MM_FREE_CALL() (free_called = 1)
+  #define MM_FREE_CALL() (free_called += 1)
   #define MM_RESET_FREE_CALL_MARKER() (free_called = 0)
   #define MM_ASSERT_FREE_CALLED(times) assert(free_called == times)
 
@@ -31,7 +31,7 @@
   #define MM_ASSERT_FREED(times) assert(freed == times)
 
   int calloc_called;
-  #define MM_CALLOC_CALL() (calloc_called = 1)
+  #define MM_CALLOC_CALL() (calloc_called += 1)
   #define MM_RESET_CALLOC_CALL_MARKER() (calloc_called = 0)
   #define MM_ASSERT_CALLOC_CALLED(times) assert(calloc_called == times)
 
